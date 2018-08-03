ab0f724ece4c1dd32b086edf78b803f83169fe97
alexandre gorius <ihkr@orange.fr>
2018-08-03 15:40:48 +0200

    App layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import SearchBar from '../containers/search_bar';
 
 const App = () => {
   return (
     <div className="app">
-      <p>React + Redux starter</p>
+      <div className="left-scene">
+        <SearchBar/>
+      </div>
+      <div className="right-scene">
+      </div>
     </div>
   );
 };
---
+++ src/containers/search_bar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  render() {
+
+    return (
+    <input type="text" className="Form-control form-search"/>
+    );
+  }
+}
+
+export default SearchBar;
